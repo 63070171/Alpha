433a83f265dc179df6ff98723a9b371c0c206fd4
63070171 <73688968+63070171@users.noreply.github.com>
2020-10-30 15:34:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php
-echo "<h2>IT@kmitl</h2>";
-echo "Hello IT!<br>";
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <title>ITF website</title>
+    </head>
+    <body>
+        <h1>
+            นาย ศุภวัฒน์ อุสาห์นนท์
+        </h1>
+        <p>ต้ม</p>
+        <br>
+        <br>
+        <h2>มุข</h2>
+        <p>ข้างขึ้น คือ 15 ค่ำ ข้างแรม คืออะไร?</p>
+        <p>เฉลย : CPU</p>
+        <img src="ram.jpg">
+        <p>ผึ้งที่ไว้ใช้เสียบกับคอมพิวเตอร์ มาจากประเทศอะไร</p>
+        <p>เฉลย : USB(US Bee)</p>
+        <img src="bee.jpg">
+        <a href="https://www.fandom.com/">ลิงที่ชอบ</a>
+    </body>
+</html>
