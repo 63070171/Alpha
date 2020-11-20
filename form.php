27c3b829aaf383038f6b845d43c340d402ee13e4
63070171 <73688968+63070171@users.noreply.github.com>
2020-11-20 22:27:39 +0700

    Update form.php

--- form.php
+++ form.php
@@ -14,7 +14,7 @@
     <div class="container">
     <?php
     $conn = mysqli_init();
-    mysqli_real_connect($conn, 'itf-63070096.mysql.database.azure.com', 'it63070096@itf-63070096', 'DTSquj72', 'itflab', 3306);
+    mysqli_real_connect($conn, 'alphadatabase1.mysql.database.azure.com', 'alpha01@alphadatabase1', 'JBIgsg12', 'Guestbook', 3306);
     if (mysqli_connect_errno($conn))
     {
         die('Failed to connect to MySQL: '.mysqli_connect_error());
