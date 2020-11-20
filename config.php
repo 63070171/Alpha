69358c92ad490d87b1a3e89d02008373fdb8e189
63070171 <73688968+63070171@users.noreply.github.com>
2020-11-20 22:45:45 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'localhost', 'root', '', 'itflab', 3306);
+mysqli_real_connect($conn, 'alphadatabase1.mysql.database.azure.com', 'alpha01@alphadatabase1', 'JBIgsg12', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
